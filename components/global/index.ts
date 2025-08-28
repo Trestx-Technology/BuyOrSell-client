f2697d331eff02a3b45b0781ea800605dfd6dd5e
Harsh <harshshinde5606@gmail.com>
2025-08-28 09:22:01 +0530

    fix:checking build

--- components/global/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Navbar } from "./Navbar";
-export { default as AISearch } from "./ai-search";
-export { default as SideMenu } from "./SideMenu";
-export { ListingCard } from "./listing-card";
-export { CardsCarousel } from "./cards-carousel";
-export { BannerCarousel } from "./banner-carousel";
-export { default as TabbedCarousel } from "./tabbed-carousel";
