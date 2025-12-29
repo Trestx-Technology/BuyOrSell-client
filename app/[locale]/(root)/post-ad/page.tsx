881f2de5f8953cc0287c8ff0fbf74da446ab3db3
Harsh <harshshinde5606@gmail.com>
2025-12-29 14:19:44 +0530

    refactor: simplify page redirection logic in post-ad component by using async function and redirect method for improved performance

--- app/[locale]/(root)/post-ad/page.tsx
+++ app/[locale]/(root)/post-ad/page.tsx
@@ -1,15 +1,10 @@
-"use client";
-import { useRouter } from "nextjs-toploader/app";
-import { useEffect } from "react";
-import { useLocale } from "@/hooks/useLocale";
+import { redirect } from "next/navigation";
 
-const Page = () => {
-  const router = useRouter();
-  const { localePath } = useLocale();
-  useEffect(() => {
-    router.push(localePath("/post-ad/select"));
-  }, [router, localePath]);
-  return null;
-};
-
-export default Page;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  redirect(`/${locale}/post-ad/select`);
+}
