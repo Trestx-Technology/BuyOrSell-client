1db360e6f52dccbbbb97b392a35a72d5271bf08e
Harsh <harshshinde5606@gmail.com>
2026-02-25 20:21:26 +0530

    fix: redirection to login even after authenticated

--- app/[locale]/(auth)/layout.tsx
+++ app/[locale]/(auth)/layout.tsx
@@ -9,6 +9,8 @@ export const metadata: Metadata = {
   description: "Authentication forms built using the components.",
 };
 
+import { AuthGuard } from "./_components/auth-guard";
+
 interface AuthRootLayoutProps {
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
@@ -18,35 +20,32 @@ const AuthRootLayout = async ({ children, params }: AuthRootLayoutProps) => {
   const { locale } = await params;
   
   return (
-    // <AuthGuard>
-    <div className="bg-white dark:bg-gray-950">
-
-
-      <div className="px-[12px] lg:px-[100px] min-h-[750px] h-screen w-screen max-w-[1280px] mx-auto flex flex-col">
-        <Link href={`/${locale}`} className=" pt-8">
-          <Image
-            src={ICONS.logo.main}
-            width={156}
-            height={49}
-            alt="logo"
-          />
-        </Link>
-        <main className="flex justify-center items-center relative gap-10 flex-1">
-          {children}
-          <div className="w-full sm:w-1/2 lg:block max-w-[530px] max-h-[603px] hidden h-full sm:h-auto">
+    <AuthGuard>
+      <div className="bg-white dark:bg-gray-950">
+        <div className="px-[12px] lg:px-[100px] min-h-[750px] h-screen w-screen max-w-[1280px] mx-auto flex flex-col">
+          <Link href={`/${locale}`} className=" pt-8">
             <Image
-              src="https://dev-buyorsell.s3.me-central-1.amazonaws.com/assets/auth-banner.jpg"
-              alt="Auth Banner"
-              width={530}
-              height={603}
-              className="w-full h-full object-cover"
+              src={ICONS.logo.main}
+              width={156}
+              height={49}
+              alt="logo"
             />
-          </div>
-        </main>
+          </Link>
+          <main className="flex justify-center items-center relative gap-10 flex-1">
+            {children}
+            <div className="w-full sm:w-1/2 lg:block max-w-[530px] max-h-[603px] hidden h-full sm:h-auto">
+              <Image
+                src="https://dev-buyorsell.s3.me-central-1.amazonaws.com/assets/auth-banner.jpg"
+                alt="Auth Banner"
+                width={530}
+                height={603}
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </main>
+        </div>
       </div>
-    </div>
-    // </AuthGuard>
-
+    </AuthGuard>
   );
 };
 
