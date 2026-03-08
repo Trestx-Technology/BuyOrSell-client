e5e9015055cd08432e0b437f1aaae5e5d3108bf5
Harsh <harshshinde5606@gmail.com>
2026-03-08 18:51:32 +0530

    style: remove inner input outline to seamlessly match outer search container on mobile

--- components/features/ai-searchbar/search-animated.tsx
+++ components/features/ai-searchbar/search-animated.tsx
@@ -46,7 +46,7 @@ export function SearchAnimated() {
         "relative flex items-center shadow rounded-lg flex-col flex-1 z-50 h-[40px]",
       )}
     >
-      <div className="h-fit absolute top-0 left-0 right-0 w-full">
+      <div className="h-fit absolute top-1/2 -translate-y-1/2 h-full left-0 right-0 w-full">
         <motion.section
           layout
           aria-label="Search"
